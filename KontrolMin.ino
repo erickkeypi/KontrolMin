103fdadf96628a26298f57fd9c4f250e7a512bf8
erickkeypi <erickrgm24@gmail.com>
2020-08-14 18:35:51 -0400

    ejemplos actualizados

--- KontrolMin.ino
+++ KontrolMin.ino
@@ -1,31 +1,46 @@
 #include "KontrolMin.h"
-#include <MemoryFree.h>
 
 KontrolMin kontrol = KontrolMin();
+
 void setup(){
   Serial.begin(115200);
   delay(2000);
-  Serial.println();
-  Serial.println(F("#####INIT#####"));
-  printMemory();
-}
+}//SETUP
 
 void loop(){
+  ////////////////////////////////
+  //Updating//
   if(Serial.available()){
     kontrol.update(Serial.read());
   }
-  kontrol.addListener("lol",test);
-}
+  //Listeners//
+  kontrol.addListener("fv",functionVoid); //send {fv} through serial
+  kontrol.addListener("fs",functionChar); //send {fs:prueba} through serial
+  kontrol.addListener("fb",functionBoolean); //send {fb:0} through serial
+  kontrol.addListener("fi",functionInt); //send {fi:12} through serial
+  kontrol.addListener("ff",functionFloat); //send {ff:3.1415} through serial
+  ////////////////////////////////
+}//LOOP
 
-
-void test(float arg){
+/////////////
+//callbacks//
+/////////////
+void functionVoid(){
+  Serial.println(F("Function without arguments"));
 }
-void printMemory(){//FUNCION QUE IMPRIME POR SERIAL LA MEMORIA DISPONIBLE
-  Serial.print(F("> MEM FREE: "));
-  Serial.print(freeMemory(), DEC);
-  Serial.print(", ");
-  float percent = freeMemory();
-  percent = percent*100/32000;
-  Serial.print(percent,2);
-  Serial.println("%");
+void functionChar(char arg[]){
+  Serial.print(F("Function with argument String: "));
+  Serial.println(arg);
+}
+void functionBoolean(bool arg){
+  Serial.print(F("Function with argument boolean: "));
+  Serial.println(arg);
+}
+void functionInt(int arg){
+  Serial.print(F("Function with argument int: "));
+  Serial.println(arg);
+}
+void functionFloat(float arg){
+  Serial.print(F("Function with argument float: "));
+  Serial.println(arg,5);
 }
--- examples/KontrolMinAll/KontrolMinAll.ino
+++ examples/KontrolMinAll/KontrolMinAll.ino
@@ -9,15 +9,14 @@ void setup(){
 void loop(){
   ////////////////////////////////
   //Updating//
-  if(Serial.available()>0){
+  if(Serial.available()){
     kontrol.update(Serial.read());
   }
   //Listeners//
   kontrol.addListener("fv",functionVoid); //send {fv} through serial
-  kontrol.addListener("fs",functionString); //send {fs:prueba} through serial
+  kontrol.addListener("fs",functionChar); //send {fs:prueba} through serial
   kontrol.addListener("fb",functionBoolean); //send {fb:0} through serial
   kontrol.addListener("fi",functionInt); //send {fi:12} through serial
-  kontrol.addListener("fl",functionLong); //send {fl:100000} through serial
   kontrol.addListener("ff",functionFloat); //send {ff:3.1415} through serial
   ////////////////////////////////
 }//LOOP
@@ -28,20 +27,16 @@ void loop(){
 void functionVoid(){
   Serial.println(F("Function without arguments"));
 }
-void functionString(String arg){
+void functionChar(char arg[]){
   Serial.print(F("Function with argument String: "));
   Serial.println(arg);
 }
-void functionBoolean(boolean arg){
+void functionBoolean(bool arg){
   Serial.print(F("Function with argument boolean: "));
   Serial.println(arg);
 }
 void functionInt(int arg){
   Serial.print(F("Function with argument int: "));
-  Serial.println(arg);
-}
-void functionLong(long arg){
-  Serial.print(F("Function with argument long: "));
   Serial.println(arg);
 }
 void functionFloat(float arg){
